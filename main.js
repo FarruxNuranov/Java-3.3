7216ab940c9ef5857ceba2b35e9a84918deb05e6
Fara <23@gmail.com>
2024-08-11 05:30:45 +0500

    fara

---
+++ main.js
@@ -0,0 +1,23 @@
+
+let x = +prompt('Введите число');
+let y = +prompt('Введите цикл')
+let res= 1
+
+ for(let i = 1; i < y;i++) {
+     if (x**y) {
+       
+        
+        res=x**y
+        
+     }
+         
+    }
+    console.log(res);
+    
+    
+    
+ 
+
+ 
+
+ 
